175d31403e4694936ddae67c086609b8b33e6648
MarkLerro <marklerro@yahoo.com>
2017-08-09 20:13:38 -0700

    First entry mod7

--- hw7.py
+++ hw7.py
@@ -2,10 +2,11 @@ from sortedcontainers import SortedDict
 
 
 def print_menu():
+    """ Prints out the user options """
     print('1. Print Users')
     print('2. Add a User')
     print('3. Remove a User')
-    print('4. Lookup a Phone Number')
+    print('4. Lookup a user name')
     print('5. Quit')
     print()
 
@@ -27,36 +28,56 @@ print_menu()
 # as long as the menu choice isn't "quit" get user options
 while menu_choice != 5:
     # get menu choice from user
-    menu_choice = int(input("Type in a number (1-5): "))
+    try:
+        menu_choice = int(input("Type in a number (1-5): "))
+    except:
+        print('Please enter a valid integer')
+        continue
 
     # view current entries
     if menu_choice == 1:
-        print("Current Users:")
-        for x, y in usernames.items():
-            print("Name: {} \tUser Name: {} \n".format(x, y))
+        try:
+            print("Current Users:")
+            for x, y in usernames.items():
+                print("Name: {} \tUser Name: {} \n".format(x, y))
+        except:
+            print('Please try again')
+            continue
 
     # add an entry
     elif menu_choice == 2:
         print("Add User")
-        name = input("Name: ")
-        username = input("User Name: ")
-        usernames[name] = username
+        try:
+            name = input("Name: ")
+            username = input("User Name: ")
+            usernames[name] = username
+        except:
+            print('Please try again')
+            continue
 
-    # remove an entry
+            # remove an entry
     elif menu_choice == 3:
         print("Remove User")
         name = input("Name: ")
-        if name in usernames:
-            pass  # delete that entry
+        try:
+            if name in usernames:
+                del (usernames[name])
+        except:
+            print('Please try again')
+            continue
 
     # view user name
     elif menu_choice == 4:
         print("Lookup User")
-        name = input("Name: ")
-        if name in usernames:
-            pass  # print the username
-        else:
-            pass  # print username not found
+        try:
+            name = input("Name: ")
+            if name in usernames:
+                print("Name: {} \tUser Name: {} \n".format(name, usernames[name]))
+            else:
+                print("username not found")
+        except:
+            print('Please try again')
+            continue
 
     # is user enters something strange, show them the menu
     elif menu_choice != 5:
